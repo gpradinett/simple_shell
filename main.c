19a17cca73138eedd9cf7e0d8eb08c7d401d2869
gpradinett <gpradinett@gmail.com>
2022-04-11 11:45:48 -0700

    fixing memory malloc errors

--- main.c
+++ main.c
@@ -3,48 +3,56 @@
 #include <errno.h>
 #include <string.h>
 #include "shell.h"
+#include "source.h"
+#include "parser.h"
+#include "executor.h"
 
 /*
  * _getline: The first function prints the prompt string.
  * print_prompt1(): reads the next line of input
  *
- *
  */
 
 int main(int argc, char **argv)
 {
-    char *cmd;
+	char *cmd;
 
-    do
-    {
+	do
+
+	{
 	/*first prints the shell's prompt*/
 	print_prompt1();
 	/*then we read a command*/
-        cmd = _getline();
+	cmd = _getline();
 	/*If there's an error reading the command, we exit the shell.*/
-        if(!cmd)
-        {
-            exit(EXIT_SUCCESS);
-        }
+	if (!cmd)
+	{
+		exit(EXIT_SUCCESS);
+	}
 	/*If the command is empty, we skip this input and continue with the loop*/
-        if(cmd[0] == '\0' || strcmp(cmd, "\n") == 0)
-        {
-            free(cmd);
-            continue;
-        }
-	/*If the command is exit, we exit the shell, Otherwise, we echo back the command,
-	free the memory we used to store the command, and continue with the loop*/
-        if(strcmp(cmd, "exit\n") == 0)
-        {
-            free(cmd);
-            break;
-        }
+	if (cmd[0] == '\0' || strcmp(cmd, "\n") == 0)
+	{
+		free(cmd);
+		continue;
+	}
+	/* If the command is exit, we exit the shell,*/
+	/* Otherwise, we echo back the command,*/
+	/* free the memory we used to store the command, and continue with the loop*/
+	if (strcmp(cmd, "exit\n") == 0)
+	{
+		free(cmd);
+		break;
+	}
+	/* printf("%s", cmd);*/
+	struct source_s src;
 
-        printf("%s", cmd);
+	src.buffer   = cmd;
+	src.bufsize  = strlen(cmd);
+	src.curpos   = INIT_SRC_POS;
+	parse_and_execute(&src);
+	free(cmd);
 
-        free(cmd);
+	} while (1);
 
-    } while(1);
-
-    exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
